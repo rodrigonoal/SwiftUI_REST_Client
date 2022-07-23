b5241cc16a886ddcbe5c9904d32adbade93fcfd5
Rodrigo Noal <rodrigonoal@gmail.com>
2022-07-23 09:15:29 -0400

    build: interceptor implemented

--- SwiftUI_REST_Client/Webservices/AuthenticationInterceptor.swift
+++ SwiftUI_REST_Client/Webservices/AuthenticationInterceptor.swift
@@ -12,6 +12,41 @@ final class AuthenticationInterceptor: Alamofire.RequestInterceptor {
         self.baseUrl = baseUrl
     }
     
+    // @escaping async context
+    private func getAccessToken(completion: @escaping (RetryResult) -> Void) -> Void{
+        let tokenEndpoint = baseUrl + "oauth/token"
+        
+        let param: Parameters = [
+            "grant_type": "password",
+            "username": userCredential.username,
+            "password": userCredential.password
+        ]
+        let tokenHeaders: HTTPHeaders = [
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic c211Y29sYTptYXRpbGR1"
+        ]
+        
+        print("Requesting access token...")
+        AF.request(tokenEndpoint, method: .post, parameters: param, encoding: URLEncoding.httpBody, headers: tokenHeaders)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseDecodable(of: AccessTokenResponse.self) { response in
+                switch response.result {
+                case.success:
+                    if let accessTokenResponse = response.value {
+                        print("The token is: " + accessTokenResponse.access_token)
+                        
+                        self.accessTokenStorage = AccessTokenStorage(access_token: accessTokenResponse.access_token, expires_in: accessTokenResponse.expires_in)
+                        
+                        return completion(.retry)
+                    }
+                case let .failure(error):
+                    print(error)
+                }
+            }
+        print("Access token has been requested...")
+    }
+    
     private func isValidTOken() -> Bool{
         if let accessToken = self.accessTokenStorage {
             return accessToken.isValidToken()
