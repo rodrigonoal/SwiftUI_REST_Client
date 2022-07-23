2c3df96d99ecff46e02699001b6ea8333aa46cc7
Rodrigo Noal <rodrigonoal@gmail.com>
2022-07-23 07:21:04 -0400

    Initial Commit

---
+++ SwiftUI_REST_Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_REST_Client
+//
+//  Created by user219835 on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_REST_Client/SwiftUI_REST_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_REST_ClientApp.swift
+//  SwiftUI_REST_Client
+//
+//  Created by user219835 on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_REST_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
